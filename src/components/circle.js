4cb3bd9c3ea1a3043e9d9b79c8d607fd6b4e21d3
meghanapulavarthy <68858341+meghanapulavarthy@users.noreply.github.com>
2020-12-05 22:28:09 +0530

    answer

--- src/components/circle.js
+++ src/components/circle.js
@@ -2,5 +2,16 @@
 
 // declare class
 
-
 // export class using module.exports
+
+const Shape = require("./shape");
+
+class Circle extends Shape {
+  constructor(color) {
+    super(color);
+  }
+  calculateArea() {
+    return 3.14 * 5 * 5;
+  }
+}
+module.exports = Circle;
--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,19 @@
 // declare class
 
 // export class using module.exports
+
+
+class Shape {
+
+  constructor() {
+      this.color = "blue";
+  }
+  drawShape() {
+      console.log("Drawing");
+  }
+  calculateArea() {
+      console.log("area");
+      return 0;
+  }
+}
+module.exports = Shape;
